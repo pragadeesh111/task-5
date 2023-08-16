f56e07b1e4e268948a6d1fc8ed1560a2571daa9e
pragadeesh111 <pragadeeshchithambaram@gmail.com>
2023-08-16 19:22:23 +0530

    task-5

--- arrowFunction.js
+++ arrowFunction.js
@@ -12,7 +12,7 @@ console.log(output.join(" ")); // output : Hi Guvi
 // c. Sum of all numbers in an array
 let sum = ((a,b)=> a+b);
 console.log(sum(2,5)); // output : 7
-
+ 
 // d. Return all the prime numbers in an array
 let array = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 let primeNumber = num => {
